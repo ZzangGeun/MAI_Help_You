4ff89310cda1c93556bb4d24d961c09637729034
geun98 <ccg70012@gmail.com>
2025-03-10 16:47:24 +0900

    dd

--- fastapi_model/model.py
+++ fastapi_model/model.py
@@ -1,14 +1,23 @@
 from langchain_openai import ChatOpenAI
 import numpy as np
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import os
 from langchain_core.prompts import ChatPromptTemplate
+
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+
+
+ # RAG 기법 사용 위해 사용자 메세지를 벡터 변환
+embeddings = HuggingFaceEmbeddings(model_name = "Upstage/solar-embedding-1-large",model_kwargs={"device": "cuda"})
+
+def get_embedding(texts):
+    vector = embeddings.embed_documents(texts)
+    return
 
 
 chat = ChatOpenAI(model="ft:gpt-4o-2024-08-06:personal::ASKX7WaZ")
