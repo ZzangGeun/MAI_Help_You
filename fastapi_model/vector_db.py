0a7f202d5b4fab72f38746aa582168183503b84b
geun98 <ccg70012@gmail.com>
2025-04-18 16:30:45 +0900

    파인튜닝 코드 추가

--- fastapi_model/vector_db.py
+++ fastapi_model/vector_db.py
@@ -3,3 +3,6 @@ from langchain_community.vectorstores.utils import DistanceStrategy
 from langchain.embeddings import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(model_name = "intfloat/multilingual-e5-large-instruct",model_kwargs={"device": "cuda"})
+
+
+def Embedder(dir_path, store_name = "FAISS_INDEX")
