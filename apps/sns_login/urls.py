823281480a68d51d3780a878855a08fea28bf330
geun98 <ccg700@naver.com>
2025-08-30 12:18:35 +0900

    협업 전 마지막 커밋

---
+++ apps/sns_login/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'sns_login'
+
+urlpatterns = [
+    # SNS 로그인 관련 URL 패턴들을 여기에 추가
+    # 예: path('login/', views.login_view, name='login'),
+]
