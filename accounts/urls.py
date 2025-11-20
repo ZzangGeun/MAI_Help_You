a2d013b6beececa536f61d9c8c26e2a793d0fc0a
ZzangGeun <ccg70012@gmail.com>
2025-11-20 17:47:52 +0900

    feat: 사용자 프로필 모델 추가 및 회원가입 API 개선: 메이플스토리 닉네임 및 넥슨 API 키 필드 추가, 로그아웃 API 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    # 회원가입 페이지 뷰
-    path('signup/', views.signup_page, name='signup_page'),
-    
     # 회원가입 API
     path('api/signup/', views.signup_api, name='signup_api'),
     
     # 로그인 API
     path('api/login/', views.login_api, name='login_api'),
+
+    # 로그아웃 API
+    path('api/logout/', views.logout_api, name='logout_api'),
 ]
