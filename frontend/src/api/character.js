f67fecefdb97cf702b6c3349efc247623757264b
ZzangGeun <ccg700@naver.com>
2026-02-07 22:51:41 +0900

    ㅇㅇ

--- frontend/src/api/character.js
+++
@@ -1,5 +0,0 @@
-import client from './client';
-
-// 캐릭터 정보 검색
-export const searchCharacter = (characterName) =>
-    client.get(`/character/api/search/?character_name=${encodeURIComponent(characterName)}`);
--- frontend/src/api/home.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-
-export const getHomeData = () => 
-  client.get('/api/home/data/');
-
-export const searchCharacter = (name) =>
-  client.get('/character/api/search/', { params: { character_name: name } });
